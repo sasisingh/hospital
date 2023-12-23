2ae427f2da4714173be10664a21e60b3119f6709
sasisingh <sasisingh6387@gmail.com>
2023-12-23 22:55:43 +0530

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,10 +28,10 @@ const Navbar = () => {
 
       <nav class="bg-blue-300 border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+          <Link class="flex items-center space-x-3 rtl:space-x-reverse">
             <img src={Logo} class="h-8" alt="Flowbite Logo" />
             <span class="self-center text-2xl text-white font-semibold whitespace-nowrap dark:text-white">Hospital</span>
-          </a>
+          </Link>
           <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button type="button" class="text-white bg-sky-600 hover:bg-blue-800   font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-400 dark:hover:bg-blue-500 dark:focus:ring-blue-800" onClick={openModal} >Book an Appointment</button>
             <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
